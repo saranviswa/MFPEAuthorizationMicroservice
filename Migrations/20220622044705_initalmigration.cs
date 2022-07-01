925f9b492c52099c48056e22c68843dc4a0e4fd5
saran viswa <bsaranviswa@gmail.com>
2022-07-01 18:54:55 +0530

    first commit

---
+++ Migrations/20220622044705_initalmigration.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AuthorizationAPI.Migrations
+{
+    public partial class initalmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BanTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Counter = table.Column<int>(type: "int", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Address", "BanTime", "Counter", "Password", "Username" },
+                values: new object[] { 1, "Mumbai", new DateTime(2000, 2, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), 0, "ADwq/exoLHtjFRAKDR/3EnOOj+wsqkpqne0lo0Tvl1PFtZ7WDOzPYQd6gUTZNCf1Zw==", "admin" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Address", "BanTime", "Counter", "Password", "Username" },
+                values: new object[] { 2, "Delhi", new DateTime(2000, 2, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), 0, "AGuu2XTe6tR4OGwcuabUf7cZSorjWOoa7iFcHULlpYGGt1kgctcBeIAiLebgq6VOCA==", "user" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
